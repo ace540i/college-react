be4d91c212f32bf4971caadde5a29314a45dfa9d
Michael Darretta <mike@tappandeveloper.com>
2025-08-11 15:23:57 -0400

    Create Warnings.tsx

---
+++ Warnings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Warnings = () => (
+  <div className="card warning">
+    <h2>Warnings</h2>
+    <p>You have not registered for the SAT yet! The registration deadline is <strong>11/27</strong>.</p>
+  </div>
+);
+
+export default Warnings;
