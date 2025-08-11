eac9730f914193e6d5d77264b07310e33ed02f4a
Michael Darretta <mike@tappandeveloper.com>
2025-08-11 15:24:37 -0400

    Create ToDos.tsx

---
+++ ToDos.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ToDos = () => (
+  <div className="card">
+    <h2>Upcoming To-Dos</h2>
+    <h3>December</h3>
+    <ul>
+      <li>Read more and practice writing skills</li>
+      <li>11/27 @ 11:00 AM: Appointment w/ Mrs. Marshall</li>
+    </ul>
+    <h3>January</h3>
+    <ul>
+      <li>Talk to parents regarding the cost of college and develop a financial plan</li>
+    </ul>
+  </div>
+);
+
+export default ToDos;
