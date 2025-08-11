128bc20222912122673e71734a295e72ad2eda00
Michael Darretta <mike@tappandeveloper.com>
2025-08-11 15:24:11 -0400

    Create ProgressTracker.tsx

---
+++ ProgressTracker.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProgressTracker = () => (
+  <div className="card">
+    <h2>Progress</h2>
+    <ul>
+      <li>Volunteer Hours: 5/10 completed</li>
+      <li>College Visits: 3/4 completed</li>
+    </ul>
+  </div>
+);
+
+export default ProgressTracker;
