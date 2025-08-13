acc850111358a70b517076b0f16229225b95c848
Michael Darretta <mike@tappandeveloper.com>
2025-08-13 18:45:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+Update your `package.json` scripts:**
+
+```json
+"scripts": {
+  "start": "node server.js",
+  "build": "react-scripts build",
+  "postinstall": "npm run build",
+  "test": "react-scripts test --env=jsdom",
+  "eject": "react-scripts eject"
+}
